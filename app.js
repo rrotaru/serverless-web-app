d57463d3fa37adb19c2f739e5a3b0719904dedb7
Robert Rotaru <Robert.Rotaru@thehartford.com>
2019-06-13 12:45:16 -0400

    add web ui

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+'use strict';
+require('dotenv').config();
+
 const express = require('express');
 var request = require('request-promise');
-
+var bodyparser = require('body-parser');
 var app = express();
+var router = express.Router();
 
 const port = 80
+
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(express.static('./public'));
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.use(require('flash')());
+
 app.get('/',(requests,response) => {
-            //callLambdaAPI();
             var options= {
                 insecure: true,
                 uri: 'https://elq9dooyng.execute-api.us-east-1.amazonaws.com/test/test  ',
@@ -23,18 +35,9 @@ app.listen(port, () => {
     console.log("running on port " + port);     
 })
 
+// Default request handler
+app.use((req, res) => {
+    res.end("404 not found");
+});
 
-
-
-function callLambdaAPI()
-{
-    var options= {
-        insecure: true,
-        uri: 'https://elq9dooyng.execute-api.us-east-1.amazonaws.com/test/test  ',
-        rejectUnauthorized: false
-    }
-    request(options)
-                .then(function (res) {
-                     console.log(res);
-    });
-}
+module.exports = app;
