6a05d841b278361e32d9696de5bd8e8ae321976f
gowthamshankar99 <Gowtham.Shankar@thehartford.com>
2019-06-13 10:49:12 -0400

    add lambda functions

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
+var request = require('request-promise');
 
 var app = express();
 
 const port = 8080
 app.get('/',(req,res) => {
-    res.send('CODE A THON!');
+            callLambdaAPI();
+            res.send('CODE A THON!');    
 })
 
 app.listen(port, () => {
-    console.log("running on port " + port);
+    console.log("running on port " + port);     
 })
+
+
+
+
+function callLambdaAPI()
+{
+    var options= {
+        insecure: true,
+        uri: 'https://elq9dooyng.execute-api.us-east-1.amazonaws.com/test/test',
+        rejectUnauthorized: false
+    }
+    request(options)
+                .then(function (res) {
+                     console.log(res);
+            });
+}
